b03d5a1000808ca790011c0e9fcaf6d640b26405
HIRATA05 <SBA22G030007@edu.sba.ac.jp>
2025-01-31 13:39:56 +0900

    VRMやMMDを使用可能にした

---
+++ Assets/Script/Scriptable/CharaData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaData : ScriptableObject
+{
+    //キャラごとのデータ
+
+    //ステータス
+    //HP
+
+    //SP
+
+    //攻撃力
+
+    //移動速度
+
+    //ジャンプ力
+
+    //技　技設定のクラスを作る　N横上下の技がある
+
+    //
+
+
+}
---
+++ Assets/Script/Scriptable/GameData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : ScriptableObject
+{
+    //プレイヤーの操作するキャラ
+
+
+    //対戦相手
+
+
+    //選択されたステージ
+
+
+
+}
